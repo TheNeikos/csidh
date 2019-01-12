6544db4f3047a0c8963ace47a4df2dbbd9e4270d
Marcel Müller <neikos@neikos.email>
2019-01-12 22:06:01 +0100

    Add better output for webasm

--- csidh-web/crate/src/lib.rs
+++ csidh-web/crate/src/lib.rs
@@ -30,34 +30,64 @@ cfg_if! {
     }
 }
 
+fn now() -> f64 {
+    web_sys::window()
+        .expect("should have a Window")
+        .performance()
+        .expect("should have a Performance")
+        .now()
+}
+
+fn write_str(text: &str) -> Result<(), JsValue> {
+    let window = web_sys::window().expect("should have a Window");
+    let document = window.document().expect("should have a Document");
+    let p: web_sys::Element = document.create_element("p")?.into();
+    p.set_inner_html(&text);
+
+    let body = document.body().expect("should have a body");
+    let body: &web_sys::Node = body.as_ref();
+    body.append_child(&p)?;
+    Ok(())
+}
+
 // Called by our JS entry point to run the example.
 #[wasm_bindgen]
 pub fn run() -> Result<(), JsValue> {
     set_panic_hook();
 
-    let window = web_sys::window().expect("should have a Window");
-    let document = window.document().expect("should have a Document");
-
     let mut rng = rand::thread_rng();
 
+    let start = now();
     let a_private = CsidhPrivateKey::generate_new(&mut rng);
     let b_private = CsidhPrivateKey::generate_new(&mut rng);
+    let end = now();
 
+    write_str(&format!("Generated private keys in {}ms", end - start));
+
+    let start = now();
     let a_public = a_private.get_public_key();
-    let b_public = b_private.get_public_key();
+    let end = now();
 
+    write_str(&format!("Generated Alice's Public Key in {}ms", end - start));
+
+    let start = now();
+    let b_public = b_private.get_public_key();
+    let end = now();
+
+    write_str(&format!("Generated Bob's Public Key in {}ms", end - start));
+
+    let start = now();
     let a_shared = a_private.get_shared_secret(&b_public);
+    let end = now();
+
+    write_str(&format!("Generated Alice's Shared Secret in {}ms:<br><pre>{:?}</pre>", end - start, a_shared));
+
+    let start = now();
     let b_shared = b_private.get_shared_secret(&a_public);
+    let end = now();
+
+    write_str(&format!("Generated Bob's Shared Secret in {}ms:<br><pre>{:?}</pre>", end - start, b_shared));
 
     assert_eq!(a_shared, b_shared);
-    let text = format!("You have succesfully created a shared secret: {:?}.", b_shared);
-
-    let p: web_sys::Node = document.create_element("p")?.into();
-    p.set_text_content(Some(&text));
-
-    let body = document.body().expect("should have a body");
-    let body: &web_sys::Node = body.as_ref();
-    body.append_child(&p)?;
-
     Ok(())
 }
